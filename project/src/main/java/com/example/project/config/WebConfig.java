db20eb7c061a417a4985011213cdc1ca2d8ce380
ParkSiHyeong <parksh4603@naver.com>
2023-10-16 18:06:28 +0900

    feat: 어드민페이지 - 회원목록 전체 틀 구현
    - 어드민페이지만의 sidebar, topbar 생성
    
    - /app/admin/memberList 의 경로를 사용했는데 html의 css,js의 경로 문제가 발생함.
    th:href="@{/경로... }" 를 사용하면 /app까지의 경로가 입력되고 그 이후의 경로를 적어주면
    어떠한 경로라도 잘 지원되어 문제 해결함.
    
    - 회원 검색, 조회 테이블, 페이지, 조회단위등의 프론트 작업 수행

--- project/src/main/java/com/example/project/config/WebConfig.java
+++ project/src/main/java/com/example/project/config/WebConfig.java
@@ -12,8 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/tables").setViewName("tables");
         registry.addViewController("/schedule").setViewName("schedule");
         registry.addViewController("/timetable").setViewName("timetable");
         registry.addViewController("/blank").setViewName("blank");
+        registry.addViewController("/admin").setViewName("admin/admin");
+        registry.addViewController("/admin/memberList").setViewName("admin/memberList");
     }
 }
