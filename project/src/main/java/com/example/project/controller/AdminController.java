451f18d9b97fe5138fb2273c979fa9e524aaf52f
ParkSiHyeong <parksh4603@naver.com>
2023-10-18 17:44:57 +0900

    feat: [back] 최 상위 부모 게시판 생성 및 조회 로직 구현
    - 전체 게시판 조회 구현
    
    - boardName, boardCategory를 통한 부모 게시판 생성 로직 구현

---
+++ project/src/main/java/com/example/project/controller/AdminController.java
@@ -0,0 +1,41 @@
+package com.example.project.controller;
+
+import com.example.project.dto.BoardDto;
+import com.example.project.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    final BoardService boardService;
+
+    public AdminController(final BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("admin/boardList")
+    public String boardList(final Model model){
+
+        List<BoardDto> boardList = boardService.selectBoardList();
+        model.addAttribute("boardList", boardList);
+        System.out.println(boardList);
+        return "admin/boardList";
+    }
+
+    @ResponseBody
+    @PostMapping ("admin/boardList")
+    public int boardAdd(@RequestParam String boardName,@RequestParam String boardCategory){
+
+        BoardDto boardDto = new BoardDto(null, boardName, boardCategory, null);
+        System.out.println(boardDto);
+        return boardService.insertBoard(boardDto);
+    }
+
+}
