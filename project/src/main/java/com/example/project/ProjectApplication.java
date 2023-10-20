1c6e7641f91173a267c309c5b3aad42579d5907c
ShinGyuJin <dlstjq5869@naver.com>
2023-10-20 13:21:03 +0900

    ProjectApplication Class 이름 변경

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages= "com.example.project")
-public class JoinProjectApplication {
+public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoinProjectApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
  
 }
