632e459561510815bb6d28e37776f3907877a856
ParkSiHyeong <parksh4603@naver.com>
2023-10-25 16:05:37 +0900

    feat: 게시판 목록 생성 및 삭제 구현
    - 생성 버튼을 통해 게시판 이름을 작성하면 새로운 게시판을 생성
    
    - 삭제할 게시판을 선택하고 삭제 버튼을 누르면 삭제되도록 구현함
    여러개의 게시판을 선택하고 삭제해도 잘 동작함
    
    DELETE FROM 테이블
    WHERE 데이터 IN (값1, 값2 ...) 구문을 통해 일괄 삭제 처리
    
    - 기본 시도교육청의 게시판 목록은 체크버튼을 생성하지 않도록 해서 삭제를 막음

---
+++ project/src/main/java/com/example/project/controller/admin/AdminBoardController.java
@@ -0,0 +1,44 @@
+package com.example.project.controller.admin;
+
+import com.example.project.dto.board.BoardDto;
+import com.example.project.dto.board.BoardPageDto;
+import com.example.project.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminBoardController {
+
+    final BoardService boardService;
+
+    public AdminBoardController(final BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/boardList")
+    public String boardList(final Model model,
+                            @RequestParam(defaultValue = "10") int displayUnit,
+                            @RequestParam(defaultValue = "1") int curPage){
+
+        final BoardPageDto boardPageDto = boardService.selectBoardPage(displayUnit, curPage);
+        model.addAttribute("boardPageDto", boardPageDto);
+        return "admin/boardList";
+    }
+
+    @ResponseBody
+    @PostMapping ("/boardList")
+    public int boardAdd(@RequestBody BoardDto boardDto){
+        return boardService.insertBoard(boardDto);
+    }
+
+    @ResponseBody
+    @DeleteMapping ("/boardList")
+    public int boardRemove(@RequestBody List<BoardDto> boardDtoList){
+        return boardService.deleteBoardList(boardDtoList);
+    }
+
+}
