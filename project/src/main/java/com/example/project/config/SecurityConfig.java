4339fca244d8e2aef8c7f05ff6be3624edba8781
ParkSiHyeong <parksh4603@naver.com>
2023-10-30 16:39:12 +0900

    feat: 시큐리티 적용
    - SecurityConfig의 configure(Httpsecurity http)에서 요청을 필터링 해서 시큐리티 적용됨
    27,28번째 줄에서 해당 경로의 권한을 확인해서 권한이 있다면 해당 요청을 수행하고
    권한이 없다면 로그인 페이지로 이동시킴
    
    - 나머지 경로는 모두 허용
    
    - 32번째 줄 밑으로 로그인 설정
    
    - 39번째 줄 밑으로 로그아웃 설정
    시큐리티에서 제공하는 로그인 뷰가 아니라 직접만든 로그인 뷰를 사용하기 위해
    .loginPage("/login"), 33번째 줄 적용 로그인 완료 시 "/loginSuccess"로 이동하고
    "/loginSuccess"는 세션값을 지정하고 "/"로 이동한다.
    
    기존의 유저 로직이 다 세션으로 작성되어 있기 때문에 이 부분을 추가 안하면
    세션부분 다 지우고, 타임리프에 sec으로 다 바꿔줘야 하기 때문에 이대로 진행함
    다 바꾸기엔 프로젝트 시간이 부족함.
    
    - PrincipalDetailService는 로그인을 하게되면 실제 DB의 정보와 id를 비교해서 권한을
    판단하는 부분이다.
    
    - UserAuthenticationProvider는 실제 로그인 인증부분 로직이다.
    입력한 id와 pw를 가져와서 DB의 정보와 비교 후 인증 반환
    
    - 이때 passwordEncoder.matches(password, user.getPassword()) / 비밀번호 비교하는 로직에서
    암호화된 값을 우측인자에 넘기는데 암호화되지 않은 값이 넘어가면 에러가 나게 되니 주의
    
    - calendar, timetable의 로그아웃 모달의 버튼 부분이 css 재정의로 생긴 문제가 있어서 부트스트랩 부분 재정의 되지 않도록 변경해서
    정상적으로 모달의 버튼크기가 적용되도록 변경
    
    - 회원가입 로직에서 암호화 적용하도록 구현

---
+++ project/src/main/java/com/example/project/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.project.config;
+
+import com.example.project.config.auth.PrincipalDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();  // csrf 비활성화
+        http.authorizeRequests()
+                    //.antMatchers("/admin/**").authenticated()   // admin 하위 경로는 모두 로그인 필요
+                    .antMatchers("/admin/**").hasRole("ADMIN")  // ADMIN 역할을 가진 유저만 접근 가능
+                    .antMatchers("/mypage/**").hasAnyRole("USER", "ADMIN")  // USER, ADMIN 역할을 가진 유저만 접근 가능
+                    .anyRequest().permitAll()   // 위의 경로를 제외한 나머지 경로는 모두 접근 허용
+                .and()
+                .formLogin()
+                    .loginPage("/login")            // 로그인 페이지 경로 지정
+                    .loginProcessingUrl("/login")   // login 주소가 호출되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                    .defaultSuccessUrl("/loginSuccess")         // 로그인 완료 시 해당 url로 이동
+                    .usernameParameter("id")        // 사용자 아이디(기본: username) 값을 id로 변경 -> UserDetails ServiceloadUserByUsername(parameter)의 파라미터 이름 변경 가능
+                    .passwordParameter("pwd")
+                    .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)                        // 세션 초기화
+                    .deleteCookies("JSESSIONID");    // 쿠키 삭제
+
+        http.sessionManagement()    // 중복 로그인
+                .maximumSessions(1) // 세션 최대 허용 수
+                .maxSessionsPreventsLogin(false);   // false: 중복 로그인 시 이전 로그인 해제
+    }
+
+}
--- project/src/main/java/com/example/project/controller/UserController.java
+++ project/src/main/java/com/example/project/controller/UserController.java
@@ -1,59 +1,42 @@
 package com.example.project.controller;
 
-import javax.servlet.http.HttpSession;
-
+import com.example.project.dto.UserDTO;
+import com.example.project.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.example.project.dto.UserDTO;
-import com.example.project.service.UserService;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
 	// @Autowire
 	final UserService userService;
+	final PasswordEncoder passwordEncoder;
 
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
+	// 로그인 성공
+	@GetMapping("/loginSuccess")
+	public String loginSuccess(Principal principal, HttpSession session, Model model) { // 로그인
 
-	// ################## Login ##################
+		UserDTO userDTO = userService.getUserById(principal.getName());
 
-	// 로그인상태 체크
-	@GetMapping("/login")
-	public String toLoginPage(HttpSession session, Model model) {
-		String id = (String) session.getAttribute("userId");
-		model.addAttribute("loginFail", "");
-		if (id != null) { // 로그인된 상태
-
-			return "redirect:./mypage/memberProfile";
-		}
-
-		return "login"; // 로그인 되지않은 상태
-
-	}
-
-	// 로그인 버튼 클랙시 산태변경
-	@PostMapping("/login")
-	public String login(String id, String pwd, HttpSession session, Model model) { // 로그인
-
-		String userId = userService.login(id, pwd);
-
-		if (userId == null) { // 로그인 실패
-
+		if (userDTO == null) { // 로그인 실패
 			model.addAttribute("loginFail", "아이디 혹은 비밀번호를 확인해주세요");
-
-			return "./login";
+			return "/login";
 		}
 
-		session.setAttribute("userId", userId);
-		return "redirect:./";
+		session.setAttribute("userId", principal.getName());
+		session.setAttribute("user", userDTO);
+		return "redirect:/";
 
 	}
 	
@@ -77,6 +60,8 @@ public class UserController {
 	@PostMapping("/signup")
 	public String signup(UserDTO userDTO, Model model) { // 화원가입
 		try {
+			// 암호화 진행
+			userDTO.setPwd(passwordEncoder.encode(userDTO.getPwd()));
 			userService.signup(userDTO);
 
 		} catch (DuplicateKeyException e) {
@@ -87,16 +72,6 @@ public class UserController {
 		}
 
 		return "redirect:/login";
-	}
-	
-	// ################## LogOut ##################
-
-	// 로그아웃
-	@PostMapping("/logout")
-	public String logout(HttpSession session) { // 로그아웃
-		session.invalidate();
-		System.out.println("세션 로그아웃");
-		return "redirect:./";
 	}
 
 }
