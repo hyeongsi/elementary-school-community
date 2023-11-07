b32637dd80cf0f79c93d87e847b6c696a514c637
ParkSiHyeong <parksh4603@naver.com>
2023-11-07 15:04:02 +0900

    feat: webclient connection pool

--- project/src/main/java/com/example/project/config/SecurityConfig.java
+++ project/src/main/java/com/example/project/config/SecurityConfig.java
@@ -14,9 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-
-	
 
     @Bean
     public PasswordEncoder passwordEncoder(){
