ff6f981c07f6dd22569614a7cbe0bb1bbd5f5878
ParkSiHyeong <parksh4603@naver.com>
2023-11-08 10:34:01 +0900

    feat: 2월1일에 학년 일제히 증가

--- project/src/main/java/com/example/project/controller/admin/AdminMemberController.java
+++ project/src/main/java/com/example/project/controller/admin/AdminMemberController.java
@@ -7,12 +7,15 @@ import com.example.project.dto.notice.CommentPageDto;
 import com.example.project.service.admin.CommentAdminService;
 import com.example.project.service.admin.MemberAdminService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -34,5 +37,12 @@ public class AdminMemberController {
     @DeleteMapping("/memberList")
     public int memberRemove(@RequestBody List<UserDTO> userDTOList){
         return memberAdminService.deleteMemberList(userDTOList);
+    }
+
+    // 2월 1일에 학생들의 학년 증가
+    @Scheduled(cron = "0 0 0 1 2 *")
+    public void increaseGrade(){
+        int n = memberAdminService.increaseGrade();
+        log.info("{}명의 학년을 업데이트했습니다.", n);
     }
 }
